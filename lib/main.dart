57393487799ed8310c4318d6d20b0d543bebc971
Cristian Paniagua <tenondecrpc@gmail.com>
2022-08-31 22:55:47 -0400

    Fix lint warnings

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Center(
         child: Text('Hello world'),
       ),
